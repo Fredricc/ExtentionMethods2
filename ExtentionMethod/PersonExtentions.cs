7b6a228cbc525795bff3a9c2d086c73455443e55
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-06 14:58:41 +0300

    Add project files.

---
+++ ExtentionMethod/PersonExtentions.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtentionMethods.ExtentionMethod
+{
+    public static class PersonExtentions
+    {
+        public static string ToFullName(this Person person)
+        {
+            return $"{person.FirstName} {person.LastName}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ExtentionMethods.ExtentionMethod;
+
+namespace ExtentionMethods
+{
+    public static class IdentificationExtentions
+    {
+        public static bool HasId(this IIdentification identification)
+        {
+            return identification.Id > 0;
+        }
+    }
+    public class Person : IIdentification
+        {
+            public Person(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+        public int Id
+        { get
+            {
+                return 7;
+            }
+        }
+    }
+
+    public interface IIdentification
+    {
+        public int Id { get; }
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                var person = new Person("Ben", "Mark");
+                Console.WriteLine($"{person.ToFullName()}");
+                Console.WriteLine($"{person.HasId()}");
+            }
+        }
+    }
